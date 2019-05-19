08c07cf1404512c4869d967be4992df816f63a50
dangen-6 <36855864+dangen-6@users.noreply.github.com>
2019-05-19 22:15:46 +0200

    Add files via upload

---
+++ Uppg 1.py
@@ -0,0 +1,32 @@
+def recept(antal):
+
+        print "Ingredienser till sockerkaka för" ,antal, "personer"
+        smor = (antal*10.0)/4
+        print "Till formen"
+        print "ca",smor,"gram smör"
+
+        strobrod = (antal*0.75)/4
+        print "ca",strobrod,"dl ströbröd"
+
+        print "Till sockerkaka"
+        agg = (antal*3)/4
+        print agg,"st agg"
+
+        strosocker = (antal*3.0)/4
+        print strosocker,"dl strösocker"
+
+        vaniljsocker = (antal*2.0)/4
+        print vaniljsocker,"tsk vaniljsocker"
+
+        bakpulver = (antal*2.0)/4
+        print bakpulver,"tsk bakpulver"
+
+        vetemjol = (antal*3.0)/4
+        print vetemjol,"tsk vetemjöl"
+
+        smor1 = (antal*75.0)/4
+        print smor1,"g smör"
+
+        vatten = (antal*1.0)/4
+        print vatten,"dl vatten"
+#-*- coding: cp1252 -*-
